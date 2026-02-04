f5392d96174c40902f7091d7cee80dad545af0b5
Justin Li <Justinnova0915@gmail.com>
2026-02-04 12:27:22 -0500

    feb-4

---
+++ feb-4.py
@@ -0,0 +1,9 @@
+# Feb 4, 2026
+
+# 1. Computer the area of a circle given its radius
+print("The area of the circle is", round(3.14*123**2, 2))
+
+# 2.
+rad = float(input("Enter the radius: "))
+area = 3.14*rad**2
+print("The area of the circle with radius", rad, "is", round(area,2),"cm sq.")
